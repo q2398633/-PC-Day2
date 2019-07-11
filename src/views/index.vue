0b93d95b553dc97f0ab8a8e73d4735d64733ed8f
耿源 <2788816975@qq.com>
2019-07-11 19:52:11 +0800

    绘制/login静态页面

---
+++ src/views/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="login-container">
+    <!-- 卡片 -->
+    <el-card class="login_box">
+      <img src="../../assets/images/logo_index.png" alt="背景" />
+      <!-- 表单 -->
+      <el-form>
+        <el-form-item>
+          <el-input placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input placeholder="请输入验证码" style="width:280px"></el-input>
+          <el-button style="float: right">发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox v-model="checked"></el-checkbox>我已阅读并同意
+          <el-link type="primary">用户协议</el-link>和
+          <el-link type="primary">隐私条款</el-link>
+        </el-form-item>
+        <el-form-item>
+          <el-button style="width: 100%" type="primary">登 录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      checked: true
+    }
+  }
+}
+</script>
+
+<style scoped lang='less'>
+.login-container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
+  .login_box {
+    width: 450px;
+    height: 350px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    img {
+      display: block;
+      width: 200px;
+      margin: 10px auto 30px;
+    }
+  }
+}
+.el-checkbox {
+  margin-right: 5px;
+}
+</style>
