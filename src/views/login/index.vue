e9e12646df041fffefd908be025cab4d956fc4ef
耿源 <2788816975@qq.com>
2019-07-11 20:44:37 +0800

    实现/login页面登录校验功能

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,12 +4,12 @@
     <el-card class="login_box">
       <img src="../../assets/images/logo_index.png" alt="背景" />
       <!-- 表单 -->
-      <el-form>
-        <el-form-item>
-          <el-input placeholder="请输入手机号"></el-input>
+      <el-form ref="loginForm" status-icon :model="loginForm" :rules="loginRules"  >
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input placeholder="请输入验证码" style="width:280px"></el-input>
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:280px"></el-input>
           <el-button style="float: right">发送验证码</el-button>
         </el-form-item>
         <el-form-item>
@@ -18,7 +18,7 @@
           <el-link type="primary">隐私条款</el-link>
         </el-form-item>
         <el-form-item>
-          <el-button style="width: 100%" type="primary">登 录</el-button>
+          <el-button style="width: 100%" type="primary" @click="login()">登 录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -28,8 +28,63 @@
 <script>
 export default {
   data () {
+    // 校验手机号
+    const checkMobile = (rule, value, callback) => {
+      // 逻辑校验
+      if (/^1[3-9]\d{9}$/.test(value)) {
+        callback()
+      } else {
+        callback(new Error('手机格式不对'))
+      }
+    }
     return {
+      // 表单对应的对象
+      loginForm: {
+        mobild: '',
+        code: ''
+      },
+      // 表单校验验证规则
+      loginRules: {
+        mobile: [
+          // 具体的校验规则,比如 是否必填 长度, 格式...
+          { required: true, message: '请输入您的手机号', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '是6位哦!请检查您输入的验证码.', trigger: 'blur' }
+        ]
+      },
       checked: true
+    }
+  },
+  methods: {
+    login () {
+      // 表单整体的校验
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          // 如果校验成功, 进行登录
+          this.$http
+            .post(
+              'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
+              this.loginForm
+            )
+            .then(res => {
+              // res是响应对象, 包含响应数据
+              const data = res.data
+              // 后台的返回的json内容已被转换成了对象
+              console.log(data)
+              // 思考: 登录成功之后,要做什么事
+              // 1.跳转到首页
+              // 2.保存登录状态
+              this.$router.push('/')
+            })
+            .catch(() => {
+              // 提示错误 使用组件 消息提示组件
+              this.$message.error('验证码错误')
+            })
+        }
+      })
     }
   }
 }
